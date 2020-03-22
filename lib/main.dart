4675f3756f48da6d473d5297788b7504b837e61d
Muhammet Can Zengin <johnzengin@Muhammets-MacBook-Pro.local>
2020-03-22 13:58:25 +0300

    Log In and Register User Interface Created

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/registration_screen.dart';
+
+void main() => runApp(PayCrypta());
+
+class PayCrypta extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        //ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
